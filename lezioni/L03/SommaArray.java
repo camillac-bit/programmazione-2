5958793f8cff9ba84bd9a90ea2a9c4aaef6763cf
Camilla Corno <camilla.corno@studenti.unimi.it>
2025-10-03 11:46:24 +0200

    Somma elementi di istanze di array

--- lezioni/L03/SommaArray.java
+++ lezioni/L03/SommaArray.java
@@ -1,12 +1,17 @@
 public class SommaArray {
-    public static void main(String[] args){
-      int[] primi = {1,2,3};
-      int somma=0;
-
-      for(int i=0;i<primi.length;i++){
-        somma+=primi[i];
-      }  
-
-      IO.println(somma);
-    }
+	public static void main(String[] args){
+		int[] primi = {1,2,3};
+		int[] secondi = {-1,-2,-3};
+		
+		IO.println(somma(primi));
+		IO.println(somma(secondi));
+	}
+	
+	static int somma(int[] valori){
+		int somma = 0;
+		for(int i=0;i<valori.length;i++){
+			somma+=valori[i];
+		}
+		return somma;
+	}
 }
