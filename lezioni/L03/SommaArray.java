02df4ea04ec337259bea323d6cdd94583f545a28
Camilla Corno <camilla.corno@studenti.unimi.it>
2025-10-03 16:03:32 +0200

    aggiunta costrutto try-with-resouces

--- lezioni/L03/SommaArray.java
+++ lezioni/L03/SommaArray.java
@@ -1,24 +1,25 @@
 import java.util.Scanner;
 
 public class SommaArray {
-	public static void main(String[] args){
-		int[] valori = new int[10];
-		Scanner sc = new Scanner(System.in); //creaimo scannere che può scannerizzare
-											 //da System.in =
-											 //= flusso di ingresso
-		int letti=0;
-		while(letti<valori.length && sc.hasNextInt()) {
-			valori[letti++] = sc.nextInt(); //"scanner dimmi il prossimo intero"
-		}
-		sc.close();
-		System.out.println(somma(valori)); //"flusso di uscita stampa"
-	}
-	
-	static int somma(int[] valori){
-		int somma = 0;
-		for(int i=0;i<valori.length;i++){
-			somma+=valori[i];
-		}
-		return somma;
-	}
+    public static void main(String[] args){
+        int[] valori = new int[10];
+        int letti = 0;
+
+        // try-with-resources: Scanner viene chiuso automaticamente alla fine
+        try (Scanner sc = new Scanner(System.in)) { 
+            while (letti < valori.length && sc.hasNextInt()) {
+                valori[letti++] = sc.nextInt(); // "scanner dimmi il prossimo intero"
+            }
+        } // sc.close() viene chiamato automaticamente qui
+
+        System.out.println(somma(valori)); // "flusso di uscita stampa"
+    }
+
+    static int somma(int[] valori){
+        int somma = 0;
+        for(int i = 0; i < valori.length; i++){
+            somma += valori[i];
+        }
+        return somma;
+    }
 }
