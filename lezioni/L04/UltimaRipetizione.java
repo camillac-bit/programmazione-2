0a3998de9b3ad5396d28e3e505ad49f257c75d82
Camilla Corno <camilla.corno@studenti.unimi.it>
2025-10-06 00:12:10 +0200

    soluzione 1

---
+++ lezioni/L04/UltimaRipetizione.java
@@ -0,0 +1,24 @@
+package lezioni.L04;
+
+/* Scrivete un programma che, data una sequenza di argomenti sulla linea di comando, 
+   emetta nel flusso di uscita l'ultimo argomento ripetuto.
+   Il flusso di uscita deve contenere l'ultimo argomento ripetuto seguito da a-capo. */
+
+public class UltimaRipetizione {
+    public static void main(String[] args) {
+        String ripetuta = "";
+        boolean found = false;
+
+        for(int i=args.length-1;i>=0 && !found;i--) {
+            for(int j=i-1;j>=0;j--) {
+                if(args[i].equals(args[j])) {
+                    ripetuta=args[i];
+                    found=true;
+                    break;
+                }
+            }
+        }
+
+        System.out.println(ripetuta);
+    }
+}
