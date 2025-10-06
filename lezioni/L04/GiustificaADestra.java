b2039284246629da9e97ae1c9a1313d96a8c8f4a
Camilla Corno <camilla.corno@studenti.unimi.it>
2025-10-06 12:21:04 +0200

    prima soluzione

---
+++ lezioni/L04/GiustificaADestra.java
@@ -0,0 +1,40 @@
+package lezioni.L04;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class GiustificaADestra {
+    public static void main(String[] args) {
+        List<String> parole = new ArrayList<>();
+        try(Scanner sc = new Scanner(System.in)){
+            while(sc.hasNext()) parole.add(sc.next());
+        }
+        printRightAligned(parole);
+    }
+
+    static int maxLength(List<String> l) {
+        int max = 0;
+        if(l.size()!=0) {
+            max=l.get(0).length();
+            for(int i=1;i<l.size();i++) {
+                if(l.get(i).length()>max) {
+                    max=l.get(i).length();
+                }
+            }
+        }
+        
+        return max;
+    }
+
+    static void printRightAligned(List<String> l) {
+        int max = maxLength(l);
+        for(String parola : l) {
+            int numSpazi=max-parola.length();
+            for(int i=0;i<numSpazi;i++){
+                System.out.print(" ");
+            }
+            System.out.println(parola);
+        }
+    }
+}
