633dbaed7d1c948848e183eff7e336e096d0a807
Camilla Corno <camilla.corno@studenti.unimi.it>
2025-11-04 22:34:32 +0100

    Esercizio 1 - Stampa semplice

---
+++ personali/StampaSemplice.java
@@ -0,0 +1,7 @@
+/* Scrivi un programma che stampi a schermo una frase a tua scelta. */
+
+public class StampaSemplice {
+    public static void main(String[] args) {
+        System.out.println("Credo in te :)");
+    }
+}
