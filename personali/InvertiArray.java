099631daed2dc2bb158118398160ce61933147e0
Camilla Corno <camilla.corno@studenti.unimi.it>
2025-11-06 12:31:22 +0100

    Esercizio 5 - Inverti array

--- personali/InvertiArray.java
+++ personali/InvertiArray.java
@@ -2,7 +2,8 @@
 public class InvertiArray {
     public static void main(String[] args){
         for(int i=args.length-1;i>=0;i--){
-            System.out.println(args[i]);
+            System.out.print(args[i] + " ");
         }
+        System.out.println();
     }
 }
