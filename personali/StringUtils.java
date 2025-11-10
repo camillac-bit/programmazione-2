0e8480b24f253e0fd90f088a50c5a62181f9b074
Camilla Corno <camilla.corno@studenti.unimi.it>
2025-11-10 18:27:39 +0100

    Esercizio 6 - Stringhe

---
+++ personali/StringUtils.java
@@ -0,0 +1,53 @@
+import java.util.Arrays;
+
+public class StringUtils {
+    
+    public static boolean sonoPalin(String s) {
+        for(int i=0;i<(s.length()/2);i++){
+            if(s.charAt(i)!=s.charAt(s.length()-1-i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static int contaLettera(String s, char ch){
+        int count = 0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)==ch){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static boolean sonoAnagrammi(String s1, String s2) {
+        if(s1.length()!=s2.length()){
+            return false;
+        }
+
+        char[] a1 = s1.toCharArray();
+        char[] a2 = s2.toCharArray();
+
+        Arrays.sort(a1);
+        Arrays.sort(a2);
+        
+        return Arrays.equals(a1,a2);
+    }
+
+    public static String cifra(String s){
+        char[] a = s.toCharArray();
+        int shift = 2;
+        for(int i=0;i<a.length;i++){
+            char c = a[i];
+            if (c >= 'a' && c <= 'z') {
+                a[i] = (char) (((c - 'a' + shift) % 26) + 'a');
+            } else if (c >= 'A' && c <= 'Z') {
+                a[i] = (char) (((c - 'A' + shift) % 26) + 'A');
+            }
+        }
+
+        return new String(a);
+    }
+
+}
